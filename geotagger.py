eebc9aa2fd47b34c74a3778300f57f719223fb09
Manish <manishsahu53@Live.com>
2018-06-20 18:46:11 +0530

    [master] added elevation support

--- geotagger.py
+++ geotagger.py
@@ -1,7 +1,5 @@
-import pexif,os
-import numpy as np
-import Tkinter, Tkconstants, tkFileDialog
-from Tkinter import * 
+import pexif
+import tkFileDialog
 import pandas as pd
 
 input_location = tkFileDialog.askdirectory(initialdir = "/",title = "Select input photos location");
@@ -9,13 +7,10 @@ output_location = tkFileDialog.askdirectory(initialdir = "/",title = "Select out
 
 csv_file = tkFileDialog.askopenfilename(initialdir = "/",title = "Select geolocation csv file",filetypes = (("csv files","*.csv"),("all files","*.*")))
 
-a= pd.read_csv(csv_file)
+a= pd.read_csv(csv_file);
 for i in range(len(a)):
     jpeg = pexif.JpegFile.fromFile(input_location + "/" + a['Images'][i]);
-    (lat,lng)=(a['Lat'][i], a['Long'][i]);
-    jpeg.set_geo(lat, lng)
-    new_file = jpeg.writeString()
-    new = pexif.JpegFile.fromString(new_file)
-    new_lat, new_lng = new.get_geo()
-    jpeg.writeFile(output_location + "/" + "geo_"+ a['Images'][i])
+    (lat,lng,alt)=(a['Lat'][i], a['Long'][i],a['Elevation'][i]);
+    jpeg.set_geo(lat, lng, alt);
+    jpeg.writeFile(output_location + "/" + a['Images'][i]);
     print("Completed " + str(i) + " images, out of " + str(len(a)) + " images");
